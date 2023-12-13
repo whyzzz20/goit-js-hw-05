e1a00853735591d6a55a030d6e7c33479501cdcb
Vitaliy <131911773+whyzzz20@users.noreply.github.com>
2023-12-13 22:06:35 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,7 @@ const sortByDescendingFriendCount = (users) => {
     return users.sort((a, b) => b.friends.length - a.friends.length);
 };
 
+// Перевірка роботи функції
 console.log(
     sortByDescendingFriendCount([
     {
